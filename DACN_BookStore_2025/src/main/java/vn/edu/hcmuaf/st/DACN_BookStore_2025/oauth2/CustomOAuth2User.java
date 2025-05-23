74e50886293243ef257ce1a54c5b34b4e441d995
lengocha <21130340@st.hcmuaf.edu.vn>
2025-05-23 18:42:02 +0700

    update ui client and funtion user( login, resgiter )

---
+++ DACN_BookStore_2025/src/main/java/vn/edu/hcmuaf/st/DACN_BookStore_2025/oauth2/CustomOAuth2User.java
@@ -0,0 +1,42 @@
+package vn.edu.hcmuaf.st.DACN_BookStore_2025.oauth2;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+    private OAuth2User oAuth2User;
+    private String clientName;
+
+    public CustomOAuth2User(OAuth2User oAuth2User, String clientName) {
+        this.oAuth2User = oAuth2User;
+        this.clientName = clientName;
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2User.getAttributes();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return oAuth2User.getAuthorities();
+    }
+
+    //tra ve username cua nguoi da dang nhap
+    @Override
+    public String getName() {
+        return oAuth2User.getAttribute("name");
+    }
+
+    public String getClientName() {
+        return this.clientName;
+    }
+}
